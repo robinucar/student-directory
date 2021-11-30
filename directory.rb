9baaca92091694d8e22c36d6315b57cf7faa6571
robinucar <mehmet.ucar@imediapro.co.uk>
2021-11-30 18:47:54 +0000

    list of students

---
+++ directory.rb
@@ -0,0 +1,19 @@
+#first we print the list of students
+puts "The students of Villiains Academy"
+puts "-------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+
+#finally we print the total number of students
+print "Overall, we have "
+print 11
+puts " great students"
