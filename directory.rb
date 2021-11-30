cda7862526838a9db20112d5c4f1ba97ad91a1bc
robinucar <mehmet.ucar@imediapro.co.uk>
2021-11-30 19:57:31 +0000

    interacitive database

--- directory.rb
+++ directory.rb
@@ -1,19 +1,16 @@
-# let's put all students into an array
-students = [
-    {name: "Darth Vader", cohort: :november},
-    {name: "Dr. Hannibal Lecter", cohort: :november},
-    {name: "Nurse Ratched", cohort: :november},
-    {name: "Michael Corleone", cohort: :november},
-    {name: "Alex DeLarge", cohort: :november},
-    {name: "The Wicked Witch of the West", cohort: :november},
-    {name: "Terminator", cohort: :november},
-    {name: "Freddy Krueger", cohort: :november},
-    {name: "The Joker", cohort: :november},
-    {name: "Joffrey Baratheon", cohort: :november},
-    {name: "Norman Bates", cohort: :november}
-]
- 
-
+def input_students
+    puts "Please enter the names of the students"
+    puts "To finish, just hit return twice"
+    students = []
+    name = gets.chomp
+    while !name.empty? do
+        students << {name: name, cohort: :november}
+        puts "Now we have #{students.count} students"
+        name = gets.chomp
+    end
+    students
+end
+students = input_students
 #and then print them
 def print_header
     puts "The students of Villiains Academy"
