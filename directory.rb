e5462039108ca46bd882b0d2fc63f62903a86a41
robinucar <mehmet.ucar@imediapro.co.uk>
2021-11-30 18:51:21 +0000

    cleaned the code

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+student_count = 11
 #first we print the list of students
 puts "The students of Villiains Academy"
 puts "-------------"
@@ -14,6 +15,4 @@ puts "Joffrey Baratheon"
 puts "Norman Bates"
 
 #finally we print the total number of students
-print "Overall, we have "
-print 11
-puts " great students"
+puts "Overall, we have #{student_count} great students"
