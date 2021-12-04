71ddd286ed4ad978c878d2d1929a52c46cf0a7be
robinucar <mehmet.ucar@imediapro.co.uk>
2021-12-04 23:28:27 +0000

    group by cohort method capitalize full name

--- directory.rb
+++ directory.rb
@@ -1,13 +1,16 @@
 
 def input_students
-    puts "Please enter the first name and cohort of the student:"
-    name, cohort = gets.split.map(&:to_sym)
-    if cohort == nil
-        cohort = 'november'.to_sym
-    end
-
+    puts "Please enter the full name of the student:"
+    name = gets.chomp.to_sym
     students = []
     while !name.empty? do
+        puts "Please type the student joined cohort:"
+        cohort = gets.chomp.to_sym
+        if cohort == ''.to_sym
+            cohort = 'november'.to_sym
+        else
+            cohort = cohort
+        end
         puts "Please enter the student age as a number:"
         age = gets.chomp.to_i
         puts "Please enter the gender of student:"
@@ -31,20 +34,36 @@ def print(names)
     index = 0
     while index < names.length 
         current_name = names[index]
-        puts "#{index + 1} First name: #{current_name[:name].capitalize()}, 
+        puts "#{index + 1} Full name: #{current_name[:name].to_s.split(/ |\_|\-/).map(&:capitalize).join(" ").to_sym}, 
             Age: #{current_name[:age]}, 
             Gender: #{current_name[:gender].capitalize()}, 
             Nationality: #{current_name[:nationality].capitalize()}, 
             Cohort: #{current_name[:cohort].capitalize()} "
         index += 1
     end
- 
 end
+
+
+
+
 #finally we print the total number of students
 def print_footer(names)
     puts "Overall, we have #{names.count} great students".center(45)
 end
 
+
 print_header
 print(students)
 print_footer(students)
+
+#grouping students by chort
+def group_by_cohort(students)
+    puts "Please type the cohort you want to see"
+    month = gets.chomp.capitalize().to_sym
+    group = students.select { |student| student[:cohort].capitalize() == month }
+    group.each do |key, value|
+        puts "#{key}: #{value}"
+    end
+end
+
+group_by_cohort(students)
