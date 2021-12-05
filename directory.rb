78c916aa465ed529e5ccaac7deb909e18dfe8603
robinucar <mehmet.ucar@imediapro.co.uk>
2021-12-05 17:00:12 +0000

    save students method

--- directory.rb
+++ directory.rb
@@ -3,6 +3,7 @@ def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Group the students by cohort"
+    puts "4. Save the list to students.csv"
     puts "9. Exit"
 end
 
@@ -19,6 +20,8 @@ def process(selection)
             show_students
         when "3"
             group_by_cohort
+        when "4"
+            save_students
         when "9"
             exit
         else
@@ -91,7 +94,17 @@ def print_footer
         puts "Overall, we have #{@students.count} great students.".center(45)
     end
 end
-
+def save_students
+    #open the file for writing
+    file = File.open("students.csv", "w")
+    #iterate over the array of students
+    @students.each do |student|
+        student_data = [student[:name], student[:age], student[:gender], student[:nationality], student[:cohort]]
+        csv_line = student_data.join(', ')
+        file.puts csv_line
+    end
+    file.close
+end
 #grouping students by chort
 def group_by_cohort
     if @students.length > 0
