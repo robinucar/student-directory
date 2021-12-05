72a112c0423184a22536d626779d4ab1209303b9
robinucar <mehmet.ucar@imediapro.co.uk>
2021-12-05 16:23:10 +0000

    interactive menu

--- directory.rb
+++ directory.rb
@@ -1,8 +1,39 @@
+@students = []
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Group the students by cohort"
+    puts "9. Exit"
+end
 
+def show_students
+    print_header
+    print_students_list
+    print_footer
+end
+def process(selection)
+    case selection
+        when "1"
+            input_students
+        when "2"
+            show_students
+        when "3"
+            group_by_cohort
+        when "9"
+            exit
+        else
+            puts "I don't know what you mean, try again"
+        end
+end
+def interactive_menu
+   loop do
+        print_menu
+        process(gets.chomp)
+   end
+end
 def input_students
     puts "Please enter the full name of the student:"
     name = gets.chomp.to_sym
-    students = []
     while !name.empty? do
         puts "Please type the student joined cohort:"
         cohort = gets.chomp.to_sym
@@ -17,27 +48,27 @@ def input_students
         gender = gets.chomp.to_s
         puts "Please enter the nationality of student:"
         nationality = gets.chomp.to_s
-        students << {name: name, age: age, gender: gender, nationality: nationality, cohort: cohort}
-        if students.count == 1
-            puts "Now we have #{students.count} student."
+        @students << {name: name, age: age, gender: gender, nationality: nationality, cohort: cohort}
+        if @students.count == 1
+            puts "Now we have #{@students.count} student."
         else
-            puts "Now we have #{students.count} students."
+            puts "Now we have #{@students.count} students."
         end
         puts "Please enter the name of another student. To finish just hit the return!"
         name = gets.chomp
     end
-    students
+    @students
 end
-students = input_students
+@students = input_students
 #and then print them
 def print_header
     puts "The students of Villiains Academy"
     puts "--------------"
 end
-def print(names)
+def print_students_list
     index = 0
-    while index < names.length 
-        current_name = names[index]
+    while index < @students.length
+        current_name = @students[index]
         puts "#{index + 1} Full name: #{current_name[:name].to_s.split(/ |\_|\-/).map(&:capitalize).join(" ").to_sym}, 
             Age: #{current_name[:age]}, 
             Gender: #{current_name[:gender].capitalize()}, 
@@ -51,27 +82,22 @@ end
 
 
 #finally we print the total number of students
-def print_footer(names)
-    if names.count == 0
+def print_footer
+    if @students.count == 0
         puts "We do not have any students yet"
-    elsif names.count == 1
-        puts "Overall, we have #{names.count} great student.".center(45)
+    elsif @students.count == 1
+        puts "Overall, we have #{@students.count} great student.".center(45)
     else
-        puts "Overall, we have #{names.count} great students.".center(45)
+        puts "Overall, we have #{@students.count} great students.".center(45)
     end
 end
 
-
-print_header
-print(students)
-print_footer(students)
-
 #grouping students by chort
-def group_by_cohort(students)
-    if students.length > 0
+def group_by_cohort
+    if @students.length > 0
         puts "Please type the cohort you want to see."
         month = gets.chomp.capitalize().to_sym
-        group = students.select { |student| student[:cohort].capitalize() == month }
+        group = @students.select { |student| student[:cohort].capitalize() == month }
         if group.length > 0
                 group.each do |key, value|
                 puts "#{key}: #{value}"
@@ -82,4 +108,4 @@ def group_by_cohort(students)
     end
 end
 
-group_by_cohort(students)
+interactive_menu
